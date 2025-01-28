8dd7214a3013d6f2d1ba3fb3f1080ee34e92eb18
Odara Oliveira <104472505+odarajara@users.noreply.github.com>
2025-01-28 02:19:39 -0300

    Update calculadora.py
    Havia esquecido de acrescentar a opção para os casos inválidos.

--- calculadora.py
+++ calculadora.py
@@ -5,11 +5,13 @@ def calculadora (op, n1, n2):
         resposta = n1 - n2
     elif(op ==3):
         resposta = n1 * n2
-    else:
+    elif(op ==4):
         resposta = n1 / n2
+    else:
+        resposta = 0
     return resposta
         
-op = int(input("Informe qual operação quer efetuar: (1)Soma (2)Subtração (3)Multiplicação (4)Divisão "))
+op = int(input("Escolha qual operação quer efetuar: (1)Soma (2)Subtração (3)Multiplicação (4)Divisão "))
 n1 = int(input("Digite o primeiro número para a operação escolhida: "))
 n2 = int(input("Digite o segundo número: "))
         
@@ -21,5 +23,7 @@ elif(op ==2):
     print("A subtração entre os números ", n1, " e " ,n2, " é ", resultado)
 elif(op ==3):
     print("A multiplicação entre os números ", n1, " e " ,n2, " é ", resultado)
-else:
+elif(op ==4):
     print("A divisão entre os números ", n1, " e " ,n2, " é ", resultado)
+else:
+    print("Você escolheu uma opção inexistente. Tente outra vez.")
